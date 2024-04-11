b7d1e4e281864d170671d8b2a6c2024c5a08b0f7
dangthanhphuc <phucdang01235@gmail.com>
2024-04-11 11:32:39 +0700

    Add dashboard view for admin

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingStore.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
